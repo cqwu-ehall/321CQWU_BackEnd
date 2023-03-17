f3d21c590e7ab35aca0d2410f995e6bcedb0f801
brian <brian@xtaolabs.com>
2023-03-18 00:29:54 +0800

    fix: lock time

--- apis/lock_time.py
+++ apis/lock_time.py
@@ -13,7 +13,7 @@ end_time = "23:55"
 async def restrict_access_middleware(request: Request, call_next):
     now = datetime.datetime.now()
     current_time = now.strftime("%H:%M")
-    if start_time <= current_time <= end_time or now.weekday() >= 5:
+    if start_time <= current_time <= end_time:
         return await call_next(request)
     else:
         return JSONResponse({"code": 1, "msg": "定时维护时间，禁止使用"}, status_code=200)
